2c1bec3d2dd301b052ba52e55ad42f5397666df6
Andy Casey <andycasey@gmail.com>
2017-03-17 10:54:32 +1100

    add unit tests

--- snob/tests/test_gaussian.py
+++ snob/tests/test_gaussian.py
@@ -49,3 +49,53 @@ class TestGaussianEstimator(unittest.TestCase):
         model.log_prior
         model.log_fisher
         model.log_data
+
+
+    def test_negative_error(self):
+
+        with self.assertRaises(ValueError):
+            gaussian.GaussianEstimator(y=[0], yerr=[-1])
+
+
+    def test_data_shape_mismatch(self):
+
+        with self.assertRaises(ValueError):
+            gaussian.GaussianEstimator(y=[0], yerr=[0.1, 0.1])
+
+        with self.assertRaises(ValueError):
+            gaussian.GaussianEstimator(y=[1, 1], yerr=0.1)
+
+
+    def test_proper_prior_on_mean(self):
+
+        with self.assertRaises(ValueError):
+            gaussian.GaussianEstimator(y=[1], mean_bounds=(None, 3))
+
+        with self.assertRaises(ValueError):
+            gaussian.GaussianEstimator(y=[1], mean_bounds=(3, None))
+
+        self.assertIsNotNone(
+            gaussian.GaussianEstimator(y=[1], mean_bounds=(None, None)))
+
+        self.assertIsNotNone(
+            gaussian.GaussianEstimator(y=[1], mean_bounds=None))
+
+
+
+
+    def test_optimization(self):
+
+        N = 5
+        y = np.random.normal(5.2, 0.4, size=N)
+        yerr = np.abs(np.random.normal(0, 0.1, size=N))
+
+        model = gaussian.GaussianEstimator(y=y, yerr=yerr)
+
+        before = model.message_length
+
+        model.optimize()
+
+        after = model.message_length
+
+        self.assertTrue(after <= before)
+        
