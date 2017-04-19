09427391a4bd3c6b3d313f7e7b829ec282a8016e
Andy Casey <andycasey@gmail.com>
2017-04-20 09:14:59 +1000

    Moving furniture

--- .ci/travis.sh
+++ .ci/travis.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 
 # Build the paper(s)
-cd articles/chemical-tagging
+cd articles/chemical-tagging-gmm
 make
 
 # Push to GitHub
@@ -9,7 +9,7 @@ if [ -n "$GITHUB_API_KEY" ]; then
   cd $TRAVIS_BUILD_DIR
   git checkout --orphan $TRAVIS_BRANCH-pdf
   git rm -rf .
-  git add -f articles/chemical-tagging/ms.pdf
-  git -c user.name='travis' -c user.email='travis' commit -m "Compiling PDF"
+  git add -f articles/*/ms.pdf
+  git -c user.name='travis' -c user.email='travis' commit -m "Compiling PDFs"
   git push -q -f https://$GITHUB_USER:$GITHUB_API_KEY@github.com/$TRAVIS_REPO_SLUG $TRAVIS_BRANCH-pdf
 fi
