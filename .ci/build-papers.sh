31b9527f7832d31ab6c202bab29c0c4f70f54d01
Andy Casey <andycasey@gmail.com>
2017-10-03 08:24:54 +1100

    Change Travis to use tectonic instead of texlive

---
+++ .ci/build-papers.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -x
+
+if [[ $BUILD_PAPERS == false ]]; then
+  return
+fi
+
+# Build the papers
+declare -a folders=("chemical-tagging-gmm" "gmm-search")
+for folder in "${folders[@]}"
+do
+   cd $TRAVIS_BUILD_DIR
+   cd "articles/$folder"
+   make
+done
+
+
+# Push to GitHub
+if [ -n "$GITHUB_API_KEY" ]; then
+  cd $TRAVIS_BUILD_DIR
+  git checkout --orphan $TRAVIS_BRANCH-pdf
+  git rm -rf .
+  git add -f paper/ms.pdf
+  git -c user.name='travis' -c user.email='travis' commit -m "building papers"
+  git push -q -f https://$GITHUB_USER:$GITHUB_API_KEY@github.com/$TRAVIS_REPO_SLUG $TRAVIS_BRANCH-pdf
+fi
--- .ci/travis.sh
+++ .ci/travis.sh
@@ -1,19 +1,39 @@
 #!/bin/bash -x
 
-# Build the paper(s)
-cd $TRAVIS_BUILD_DIR/articles/chemical-tagging-gmm
-make
-#cd $TRAVIS_BUILD_DIR/articles/nips-2017
-#make
-cd $TRAVIS_BUILD_DIR/articles/gmm-search
-make
-
-# Push to GitHub
-if [ -n "$GITHUB_API_KEY" ]; then
-  cd $TRAVIS_BUILD_DIR
-  git checkout --orphan $TRAVIS_BRANCH-pdf
-  git rm -rf .
-  git add -f articles/*/ms.pdf
-  git -c user.name='travis' -c user.email='travis' commit -m "Compiling PDFs"
-  git push -q -f https://$GITHUB_USER:$GITHUB_API_KEY@github.com/$TRAVIS_REPO_SLUG $TRAVIS_BRANCH-pdf
+# If building the paper, do that here
+if [[ $TEST_LANG == paper ]]
+then
+  if git diff --name-only $TRAVIS_COMMIT_RANGE | grep 'articles/'
+  then
+    echo "Building the paper..."
+    export BUILD_PAPERS=true
+    source "$( dirname "${BASH_SOURCE[0]}" )"/setup-tectonic.sh
+    return
+  fi
+  export BUILD_PAPERS=false
+  return
 fi
+
+# If we make it here then we're testing Python
+
+# http://conda.pydata.org/docs/travis.html#the-travis-yml-file
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+  wget https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda.sh;
+else
+  wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
+fi
+bash miniconda.sh -b -p $HOME/miniconda
+export PATH="$HOME/miniconda/bin:$PATH"
+
+# Conda Python
+hash -r
+conda config --set always_yes yes --set changeps1 no
+conda update -q conda
+conda info -a
+conda create --yes -n test python=$PYTHON_VERSION
+source activate test
+conda install -c conda-forge numpy=$NUMPY_VERSION setuptools pytest pytest-cov pip
+pip install coveralls
+
+# Install code.
+python setup.py develop
