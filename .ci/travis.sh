f7c59f2bf819413d187d116aea895772f7c9abcb
Andy Casey <andycasey@gmail.com>
2017-10-02 16:48:41 +1100

    get travis to build all pdfs (hopefully)

--- .ci/travis.sh
+++ .ci/travis.sh
@@ -13,7 +13,7 @@ if [ -n "$GITHUB_API_KEY" ]; then
   cd $TRAVIS_BUILD_DIR
   git checkout --orphan $TRAVIS_BRANCH-pdf
   git rm -rf .
-  git add -f articles/*/ms.pdf
+  git add -f articles/chemical-tagging-gmm/ms.pdf articles/gmm-search/ms.pdf
   git -c user.name='travis' -c user.email='travis' commit -m "Compiling PDFs"
   git push -q -f https://$GITHUB_USER:$GITHUB_API_KEY@github.com/$TRAVIS_REPO_SLUG $TRAVIS_BRANCH-pdf
 fi
